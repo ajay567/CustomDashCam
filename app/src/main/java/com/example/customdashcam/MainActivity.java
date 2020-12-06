0f47028211fbdfe806f6f4eb5728ed37a104ba7e
hassanyakef <yhassan@vt.edu>
2020-12-05 20:11:39 -0500

    Show the video files on the gallery page.

--- app/src/main/java/com/example/customdashcam/MainActivity.java
+++ app/src/main/java/com/example/customdashcam/MainActivity.java
@@ -1,19 +1,42 @@
 package com.example.customdashcam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
+
+    private boolean permission;
+    private File storage;
+    private String[] allPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // load data here
+        // Get all storage areas path
+        allPath = StorageUtil.getStorageDirectories(this);
+
+        for(String path: allPath) {
+            storage = new File(path);
+            Method.load_Directory_Files(storage);
+        }
+
         setContentView(R.layout.activity_main);
+
+
 
         final Button recordPage = findViewById(R.id.HomeRecordParam);
 
